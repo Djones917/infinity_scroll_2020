fc750691a8016d94b08782556b8591634b062a46
Derek <nightwing917@hotmail.com>
2020-09-13 20:42:01 -0600

    working on async function for getting photos

--- script.js
+++ script.js
@@ -3,3 +3,18 @@
 const count = 10;
 const apiKey = '';
 const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
+
+// Get photos from api
+async function getPhotos() {
+    try {
+      const response = await fetch(apiUrl);
+      const data = await response.json();
+      console.log(data);
+    } catch (error) {
+       // Catch error here
+
+    }
+}
+
+// ON LOAD
+getPhotos();
