f5370ee2f2e78a2a0366520d387c2f1a7323c821
Derek <nightwing917@hotmail.com>
2020-09-19 18:54:21 -0600

    showing live images

--- script.js
+++ script.js
@@ -10,16 +10,19 @@ let photosArray = [];
 // Create elements for links, photos add to DOM
 function displayPhotos() {
   // Run function for each object in photosArray
-  photosArray.forEach((ohoto) => {
+  photosArray.forEach((photo) => {
     // Create <a> to link to unsplash
     const item = document.createElement('a');
     item.setAttribute('href', photo.links.html);
     item.setAttribute('target', '_blank');
     // Create img for photo
     const img = document.createElement('img');
-    ime.setAttribute('src', photo.urls.regular);
+    img.setAttribute('src', photo.urls.regular);
     img.setAttribute('alt', photo.alt_description);
     img.setAttribute('title', photo.alt_description);
+    // Put img inside <a>, then put both inside imageContainer Element
+    item.appendChild(img);
+    imageContainer.appendChild(item);
   });
 } 
 
